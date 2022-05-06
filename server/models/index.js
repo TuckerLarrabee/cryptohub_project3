373f83c710acf15cdbd35941ddbb60dfaa3c9a1b
tuckl <tuckerlarrabee13@gmail.com>
2022-05-06 12:03:41 -0400

    back end boiler plate

---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Crypto = require('./Crypto')
+
+module.exports = { User, Crypto };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+  }
+
+  type Crypto {
+    _id: ID
+    cryptocurrencyName: String
+    username: [User]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    crypto: [Crypto]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
