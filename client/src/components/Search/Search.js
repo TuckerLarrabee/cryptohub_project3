a0f281914634bbe16a7da43ef2d3034a56791ac7
tuckl <tuckerlarrabee13@gmail.com>
2022-05-07 15:00:23 -0400

    styling, api call for coin prices and mapping of cards

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,17 +1,38 @@
 import React from "react";
+import axios from 'axios';
+import { useState } from "react";
+import { useEffect } from "react";
+import { QUERY_CRYPTOS} from "../../utils/queries"
+import { useQuery } from "@apollo/client";
 
 function Search() {
+    const { loading, data } = useQuery(QUERY_CRYPTOS);
+    const cryptos = data?.cryptos || [];
+    console.log(loading, cryptos);
+
+
+  // const [cryptos, setCryptos] = useState([]);
+
+  // useEffect(() => {
+  //   const loadCryptos = async () > {
+  //     const response = await
+  //   }
+  // })
+
+
+
   return (
     <form className="py-2 mx-2" id="cryptoForm">
             <div className="searchDiv">
                 <label>
-                  <h5 class="center">Search for a Cryptocurreny:</h5>
+                  <h5 className="center">Search for a Cryptocurreny:</h5>
                 </label>
                 <input
+                  type="text"
                   name="cryptoName"
                   id="cryptoName"
                   placeholder="Litecoin"
-                  class="input"
+                  className="input"
                 />
                 
                   <button className="mx-1" type="submit" id="searchBtn">
