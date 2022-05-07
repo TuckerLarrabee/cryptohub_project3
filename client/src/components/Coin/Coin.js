17c5e0b3afb88f2c6c4cd437df3456538b9c09e0
tuckl <tuckerlarrabee13@gmail.com>
2022-05-07 17:18:26 -0400

    Styling / coin component changes

--- client/src/components/Coin/Coin.js
+++ client/src/components/Coin/Coin.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Coin({ name, icon, price, symbol }) {
+
+    let twoDec = price.toFixed(2);
+    let internationalNumberFormat = new Intl.NumberFormat('en-US');
+    let comma = internationalNumberFormat.format(twoDec);
+    
+    
+
   return (
     <div className="coin">
       <h1> {name}</h1>
       <img src={icon} />
-      <h3> Price: {"$ " + price.toFixed(2)}</h3>
+      <h3> Price: {"$" + comma}</h3>
       <h3> Symbol: {symbol}</h3>
     </div>
   );
