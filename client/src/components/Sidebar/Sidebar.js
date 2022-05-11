431b3a65b2318af69139eff779c2272a830306f5
Jared Long <longja2015@gmail.com>
2022-05-10 22:08:17 -0400

    test for sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+const Drop = styled.div`
+    background: #fafafa;
+    height: 80px;   
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const DropIcon = styled(Link)`
+    margin-left: 2rem;
+`
 
 const Sidebar = () => {
-    return <div>Sidebar</div>
+    return <>
+    <Drop>
+        <DropIcon to='#'>
+            <FaIcons.FaBars />
+        </DropIcon>
+    </Drop>
+    </>
 };
 
 export default Sidebar;
