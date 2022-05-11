102450b35e36dd9a2231aa433d374f626278371f
Jared Long <longja2015@gmail.com>
2022-05-10 22:42:03 -0400

    onclick sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
@@ -14,16 +14,51 @@ const Drop = styled.div`
 
 const DropIcon = styled(Link)`
     margin-left: 2rem;
-`
+    font-size: 2rem; 
+    height: 80px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const SidebarNav = styled.nav`
+    background: #fafafa;
+    width: 250px;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+    transition: 350ms;
+    z-index: 10;
+`;
+
+const SidebarWrap = styled.div`
+    width: 100%;
+`;
 
 const Sidebar = () => {
-    return <>
+const [sidebar, setSidebar] = useState(false)
+const showSidebar = () => setSidebar(!sidebar)
+
+
+    return (
+    <>
     <Drop>
         <DropIcon to='#'>
-            <FaIcons.FaBars />
+            <FaIcons.FaBars onClick={showSidebar} />
         </DropIcon>
     </Drop>
+    <SidebarNav sidebar={sidebar}>
+        <SidebarWrap>
+        <DropIcon to='#'>
+            <AiIcons.AiOutlineClose onClick={showSidebar} />
+        </DropIcon>
+        </SidebarWrap>
+    </SidebarNav>
     </>
+    );
 };
 
 export default Sidebar;
