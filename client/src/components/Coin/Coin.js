9477eed043810bf6ba2776c510b19345de8c35bc
tuckl <tuckerlarrabee13@gmail.com>
2022-05-10 21:50:29 -0400

    getting favorited coins into client side

--- client/src/components/Coin/Coin.js
+++ client/src/components/Coin/Coin.js
@@ -1,63 +1,79 @@
 import React from "react";
 import { useState } from "react";
-import { FAVORITE_COIN } from "../../utils/mutations";
+import { FAVORITE_COIN, UNFAVORITE_COIN } from "../../utils/mutations";
+// import { QUERY_ME } from "../../utils/queries";
 import { useMutation } from "@apollo/client";
 
-function Coin({ name, icon, price, symbol, marketCap, id }) {
+function Coin({ name, icon, price, symbol, marketCap, id, arr }) {
+  const [addFavorite] = useMutation(FAVORITE_COIN);
+  const [removeFavorite] = useMutation(UNFAVORITE_COIN);
 
-    const [addFavorite] = useMutation(FAVORITE_COIN);
+  let uniqueCoins = [...new Set(arr)];
+  console.log(uniqueCoins);
 
-    const [isActive, setActive] = useState(false);
-    const toggleClass = (id) => {
-        console.log(id)
-        setActive(!isActive);
-      };
+  const [isActive, setActive] = useState(false);
 
-    let twoDec = price.toFixed(2);
-    let internationalNumberFormat = new Intl.NumberFormat('en-US');
-    let comma = internationalNumberFormat.format(twoDec);
-    
-    function nFormatter(num) {
-        if (num >= 1000000000) {
-           return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
-        }
-        if (num >= 1000000) {
-           return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
-        }
-        if (num >= 1000) {
-           return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
-        }
-        return num;
-   }
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
 
-   let cap = nFormatter(marketCap);
-   
+  let twoDec = price.toFixed(2);
+  let internationalNumberFormat = new Intl.NumberFormat("en-US");
+  let comma = internationalNumberFormat.format(twoDec);
 
-   function handleFavorite () {
+  function nFormatter(num) {
+    if (num >= 1000000000) {
+      return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
+    }
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+    }
+    if (num >= 1000) {
+      return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+    }
+    return num;
+  }
 
-     toggleClass(id);
+  let cap = nFormatter(marketCap);
 
-      addFavorite(
-        {
-          variables:
-            {
-              cryptocurrency: id
-            }
-        }
-      ) 
-   }
+  function handleFavorite(event) {
+    toggleClass();
+    console.log(event.target.classList[0]);
+
+    if (event.target.classList[0] === "liked") {
+      removeFavorite({
+        variables: {
+          cryptocurrency: id,
+        },
+      });
+    } else {
+      addFavorite({
+        variables: {
+          cryptocurrency: id,
+        },
+      });
+    }
+  }
 
   return (
     <div className="coin">
-        <h1 className="px-1"> {name} {"(" + symbol + ")"} </h1>
-        <img src={icon} />
-        <div className="like-container">
-            <div>
-                <h3 className="px-3"> Price: {"$" + comma}</h3>
-                <h3 className="px-3">Market Cap: {"$" + cap}</h3>
-            </div>
-            <button className={isActive ? 'liked like-button': 'like-button'} onClick={() => handleFavorite()}></button>
+      <h1 className="px-1">
+        {" "}
+        {name} {"(" + symbol + ")"}{" "}
+      </h1>
+      <img src={icon} />
+      <div className="like-container">
+        <div>
+          <h3 className="px-3"> Price: {"$" + comma}</h3>
+          <h3 className="px-3">Market Cap: {"$" + cap}</h3>
         </div>
+        <button
+          className={
+            uniqueCoins.includes(id) ? "liked like-button" : "like-button"
+          }
+          onClick={handleFavorite}
+        ></button>
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,40 @@
 import React from "react";
 import Search from "../components/Search/Search";
 import Axios from "axios";
-import Coin from "../components/Coin/Coin"
-import { useEffect, useState } from 'react';
-
+import Coin from "../components/Coin/Coin";
+import { useEffect, useState } from "react";
+import { QUERY_ME } from "../utils/queries";
+import { useQuery } from "@apollo/client";
+let coinData;
+const coinArr = [];
 
 const Home = () => {
   const [listOfCryptos, setListOfCryptos] = useState([]);
   const [searchWord, setSearchWord] = useState("");
 
+  const { loading, data } = useQuery(QUERY_ME);
+
+  if (!loading) {
+    data.me.coins.map((coin) => {
+      coinArr.push(coin.cryptocurrency);
+    });
+  }
+
+  console.log(coinArr);
+
   useEffect(() => {
     Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
       (response) => {
         setListOfCryptos(response.data.coins);
-        console.log(response.data.coins)
+        console.log(response.data.coins);
       }
     );
   }, []);
 
   const filteredCoins = listOfCryptos.filter((coin) => {
     return coin.name.toLowerCase().includes(searchWord.toLowerCase());
-  }); 
-  
+  });
+
   return (
     <div className="Home">
       <div className="py-2 col-2 cryptoHeader">
@@ -50,6 +63,7 @@ const Home = () => {
                 marketCap={coin.marketCap}
                 key={coin.name}
                 id={coin.id}
+                arr={coinArr}
               />
             );
           })}
