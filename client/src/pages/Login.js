51f413bb494e8bac9f22763e13d4a698a9b76cd2
Jared Long <longja2015@gmail.com>
2022-05-12 22:39:26 -0400

    login styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -30,7 +30,7 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
+    <div className="login">
       <Link to="/signup">← Go to Signup</Link>
 
       <h2>Login</h2>
