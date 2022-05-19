97a8b555af0d33df35843c59491e5787aa0c462a
tuckl <tuckerlarrabee13@gmail.com>
2022-05-19 17:16:41 -0400

    build version for heroku

---
+++ client/build/precache-manifest.d346b92c38832ba6f76620909d2af469.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1e67005a350e6c131f54ce900fad7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a9deb9a217a9226d555",
+    "url": "/static/css/main.e597ddd8.chunk.css"
+  },
+  {
+    "revision": "754dd6fda31f2facd25f",
+    "url": "/static/js/2.6f12a43b.chunk.js"
+  },
+  {
+    "revision": "c3a99cfe772ea5d64c63972f94da6321",
+    "url": "/static/js/2.6f12a43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a9deb9a217a9226d555",
+    "url": "/static/js/main.e16ab196.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "5fc130014b6dade6dcc1660367b86e21",
+    "url": "/static/media/CryptoHubLogo.5fc13001.png"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
